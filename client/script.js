1465f75d5a5089d720de95331ea6cdcb1da85506
Nick Correa <nickcorreagarcia@gmail.com>
2021-07-31 18:25:51 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,15 +1,12 @@
 import Landpage from "./assets/scripts/Landpage.js";
-import ProductsService from "./assets/scripts/services/products_service.js";
+import CategoriesService from "./assets/scripts/services/categories_service.js";
 import STORE from "./assets/scripts/store.js";
 
 async function init() {
   const landpage = new Landpage(".js-container");
 
-  const productsService = new ProductsService();
-  STORE.products = await productsService.list();
-  console.log(STORE.products);
-  // console.log(landpage.parentSelector);
-  // console.log(landpage.render());
+  const categoriesService = new CategoriesService();
+  STORE.categories = await categoriesService.list();
   landpage.render();
 }
 
