36881999dd3cc1d22f39c8cf4369592bb6b7a580
Nick Correa <nickcorreagarcia@gmail.com>
2021-07-31 18:27:07 -0500

    Create Categories Service

---
+++ client/assets/scripts/services/categories_service.js
@@ -0,0 +1,18 @@
+import { apiFetch, BASE_URL } from "./api_fetch.js";
+
+function CategoriesService() {
+  if (!CategoriesService.instance) {
+    CategoriesService.instance = this;
+  }
+  return CategoriesService.instance;
+}
+
+CategoriesService.prototype.list = () =>
+  apiFetch(`${BASE_URL}/categories`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+export default CategoriesService;
